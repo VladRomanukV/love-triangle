ed73d2021c27f669d51a5f6961064f7dee192e4f
ValdRomanuV <vladromanuk12vv@gmail.com>
2019-02-23 11:18:28 +0300

    Love :3

--- src/index.js
+++ src/index.js
@@ -4,4 +4,15 @@
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
   // your implementation
+  	var sum = 0;
+    preferences.unshift(0);
+   
+   for(var i = 1; i < preferences.length; i++){
+       var b = preferences[i];
+       var c = preferences[b];
+       
+       sum += i == preferences[c];
+   }
+   sum = sum/3
+   return Math.round(sum);
 };
